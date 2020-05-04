86aa0dddb0e4888561dd1fd12ffb178e93db572d
arubinofaux <arubinofaux@gmail.com>
2020-05-04 15:43:48 -0400

    update api host

--- hodlwallet/src/Platform/BRAPIClient.swift
+++ hodlwallet/src/Platform/BRAPIClient.swift
@@ -70,7 +70,7 @@ open class BRAPIClient : NSObject, URLSessionDelegate, URLSessionTaskDelegate, B
     var proto = "https"
     
     // host is the server(s) on which the API is hosted
-    var host = "precio.bitstop.co"
+    var host = "durban.herokuapp.com"
     
     // isFetchingAuth is set to true when a request is currently trying to renew authentication (the token)
     // it is useful because fetching auth is not idempotent and not reentrant, so at most one auth attempt
